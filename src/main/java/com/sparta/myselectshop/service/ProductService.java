1b7bc44b204ba654c2707d6115904a32abd128e8
sdongpil <pildong94@naver.com>
2024-11-04 15:43:09 +0900

    feat: 관심상품 조회 api 개발

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -9,6 +9,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
@@ -34,5 +38,13 @@ public class ProductService {
         product.update(requestDto);
 
         return new ProductResponseDto(product);
+    }
+
+    public List<ProductResponseDto> getProducts() {
+        List<Product> all = productRepository.findAll();
+
+        return all.stream()
+                .map(ProductResponseDto::new)
+                .toList();
     }
 }
